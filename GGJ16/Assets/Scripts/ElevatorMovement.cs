08a6cb611d8bfc0470e7f807f66903678bbf6309
thegreatpl <imperialjustinian@gmail.com>
2016-01-30 14:13:00 +0000

    added basic elevator movement script, but not done anything with it.

---
+++ GGJ16/Assets/Scripts/ElevatorMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ElevatorMovement : MonoBehaviour {
+
+    public List<Vector2> MovementLocations = new List<Vector2>();
+
+    int nextLocation = 0; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
