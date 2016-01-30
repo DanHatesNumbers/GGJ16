0caa16825b52f6de4f3f2f0b94a51ca6895b7783
Daniel Wright <danwright91@gmail.com>
2016-01-30 22:08:37 +0000

    Changed fireball forces and camera FOV

--- GGJ16/Assets/Scripts/PlayerMovement.cs
+++ GGJ16/Assets/Scripts/PlayerMovement.cs
@@ -149,7 +149,7 @@ public class PlayerMovement : NetworkBehaviour {
             position.x += Player.GetComponent<Collider2D>().bounds.size.x;
         }
         var fireball = (GameObject)Instantiate(Fireball, position, Quaternion.Euler(new Vector3(0, 0, 0)));
-        var fireballVelocity = FacingLeft ? new Vector2(-500f, 0f) : new Vector2(500f, 0f);
+        var fireballVelocity = FacingLeft ? new Vector2(-750f, 0f) : new Vector2(750f, 0f);
         fireball.GetComponent<Rigidbody2D>().AddForce(fireballVelocity, ForceMode2D.Impulse);
 
         NetworkServer.Spawn(fireball);
