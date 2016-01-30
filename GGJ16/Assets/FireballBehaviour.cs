77c01443c1701426bc78327a686523d7e32cf9b8
Daniel Wright <danwright91@gmail.com>
2016-01-30 13:30:19 +0000

    Added fireball despawn and add force on collide with non local player logic. Made sure fireballs spawn outside of player collider

---
+++ GGJ16/Assets/FireballBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class FireballBehaviour : MonoBehaviour {
+    // Use this for initialization
+    public float FireballForceScale;
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(String.Format("Collided with {0}", col.gameObject.name));
+        if(col.gameObject.name == "Player" && !col.gameObject.GetComponent<PlayerMovement>().hasAuthority)
+        {
+            var dir = this.gameObject.transform.position - col.gameObject.transform.position;
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(dir * FireballForceScale, ForceMode2D.Impulse);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- GGJ16/Assets/PlayerMovement.cs
+++ GGJ16/Assets/PlayerMovement.cs
@@ -118,7 +118,9 @@ public class PlayerMovement : NetworkBehaviour {
 
     void SpawnFireball()
     {
-        var fireball = (GameObject)Instantiate(Fireball, Player.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        var position = Player.transform.position;
+        position.x += Player.GetComponent<Collider2D>().bounds.size.x;
+        var fireball = (GameObject)Instantiate(Fireball, position, Quaternion.Euler(new Vector3(0, 0, 0)));
         var fireballVelocity = FacingLeft ? new Vector2(-5f, 0f) : new Vector2(5f, 0f);
         fireball.GetComponent<Rigidbody2D>().velocity = fireballVelocity;
         LastFireTime = Time.time;
