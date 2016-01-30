981ba0be8a431ba3c50d56b6d90a9c48b3f79f2f
ferrittafh <patrick.merritt@ntu.ac.uk>
2016-01-30 15:32:26 +0000

    destroyable tiles script sync

---
+++ GGJ16/Assets/Tiles/DestroyableTerrain.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyableTerrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D Col)
+    {
+        if ((Col.gameObject.name == "LavaBottom") || (Col.gameObject.name == "KillZ"))
+        {
+            Destroy(this.gameObject);
+        }
+        if (Col.gameObject.name == "FireBall")
+        {
+            if (this.GetComponent<Rigidbody2D>() == null)
+            {
+                this.gameObject.AddComponent<Rigidbody2D>();
+                
+            }
+            Destroy(Col.gameObject);
+        }
+    }
+}
