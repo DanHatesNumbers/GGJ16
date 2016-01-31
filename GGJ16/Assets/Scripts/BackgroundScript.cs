338db1e4bf46a25260090fd6e60d779e9e67f778
thegreatpl <imperialjustinian@gmail.com>
2016-01-31 14:10:12 +0000

    now have perlin noise background! Yay! not too different yet, need to fuck about with values.

--- GGJ16/Assets/Scripts/BackgroundScript.cs
+++ GGJ16/Assets/Scripts/BackgroundScript.cs
@@ -5,23 +5,38 @@ public class BackgroundScript : MonoBehaviour {
 
     public Vector2 MapSize = new Vector2(1, 1);
 
-    public int DividerLevel = 1; 
+    public int DividerLevel = 1;
+
+    public int PixelsToUnits = 1;
+
+    public int scale = 10; 
 
 	// Use this for initialization
 	void Start () {
+        Vector2 mapSize = new Vector2(MapSize.x * PixelsToUnits, MapSize.y * PixelsToUnits);
+        Texture2D texture = new Texture2D((int)mapSize.x, (int)mapSize.y, TextureFormat.ARGB32, false); 
 
-        Texture2D texture = new Texture2D((int)MapSize.x, (int)MapSize.y); 
-
-        for (int xdx = 0; xdx < MapSize.x; xdx++)
+        for (int xdx = 0; xdx < mapSize.x; xdx++)
         {
-            for (int ydx = 0; ydx < MapSize.y; ydx++)
+            for (int ydx = 0; ydx < mapSize.y; ydx++)
             {
-                texture.SetPixel(xdx, ydx, Color.black); 
+                float noiseVal = Mathf.PerlinNoise(xdx + 0.01f, ydx + 0.01f);
+                //Debug.Log("Noise Val for " + xdx + ", " + ydx + "is " + noiseVal); 
+                texture.SetPixel(xdx, ydx, new Color(noiseVal, noiseVal, noiseVal)); 
+
+                //if (xdx % 2 != 0)
+                //{
+                //    texture.SetPixel(xdx, ydx, Color.white);
+                //}
+                //else
+                //{
+                //    texture.SetPixel(xdx, ydx, Color.black);
+                //}
             }
         }
 
         texture.Apply();
-        GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, new Rect(0, 0, MapSize.x, MapSize.y), new Vector2(), 1);
+        GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, new Rect(0, 0, MapSize.x, MapSize.y), new Vector2(), PixelsToUnits);
 
        // GetComponent<S>()material.mainTexture = texture; 
 	}
